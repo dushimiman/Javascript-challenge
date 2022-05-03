757241b2215b3c26b20da206b98b2d0f7c9a6942
dushimimana diane <dushimedian12@gmail.com>
2022-05-03 16:20:18 +0200

    add other question

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ let sum = [100,200,10,5,500,100].reduce(function(previousValue, currentValue){
 }, 100);
  console.log("SUM:", sum)
 
+console.log(products)
+// for each
+products.forEach(item => {
+    console.log(`the price for ${item.product} is ${item.price}`);
+})
+// Map
+const newPrice = products.map(item =>{
+    return {product:item.name, price:item.price * 2};
+})
+console.log(newPrice);
+// Filter
+const productList = products.filter(item => {
+    return item.product === 'Book';
+})
+console.log(productList);
+//sort
+ const sortedArray = products.sort((a,b) =>{
+     return b.price.localeCompare(a.price)
+ })
+ console.log(sortedArray)
